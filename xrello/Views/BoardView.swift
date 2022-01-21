89a631fd65edb34c0a325045ad2a34847121ee7c
aRobihamanto <rrobie44@gmail.com>
2022-01-21 15:27:25 +0800

    Create BoardView.swift

---
+++ xrello/Views/BoardView.swift
@@ -0,0 +1,24 @@
+//
+//  BoardView.swift
+//  xrello
+//
+//  Created by Robihamanto on 2022/1/21.
+//
+
+import SwiftUI
+
+let boardListBackgroundColor = Color(uiColor: UIColor(red: 0.92, green: 0.92, blue: 0.94, alpha: 1))
+
+
+struct BoardView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct BoardView_Previews: PreviewProvider {
+    static var previews: some View {
+        BoardView()
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
